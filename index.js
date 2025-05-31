891182e40e7266f1fc4954f59313a26aae29ef50
metax <85768079+pplbgtz@users.noreply.github.com>
2025-06-01 02:35:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ const arrayToHex = (bf) => {
     return [...new Uint8Array(bf)].map(b => b.toString(16).padStart(2, "0")).join(""); 
 };
 
-const getRand = (account, lasttxbuff) => {
-    const arr = new Uint8Array(24);
-    for (let i = 0; i < 8; i++) arr[i] = account[i];
-    for (let i = 8; i < 16; i++) arr[i] = lasttxbuff[i-8];
-    return arr;
-};
-
 const charToSymbol = function (c) {
     if (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) return (c - 'a'.charCodeAt(0)) + 6;
     if (c >= '1'.charCodeAt(0) && c <= '5'.charCodeAt(0)) return (c - '1'.charCodeAt(0)) + 1;
@@ -41,17 +34,31 @@ const difficulty = 0;
 let start = Date.now(); // start time
 let accountBuffer = pushName(account);
 let lastMineBuffer = Array.from(Buffer.from(lastminetx, 'hex'));
-let rand = getRand(accountBuffer, lastMineBuffer); // get rand uint 8 array ( 0-16 ) from Account name and last mine transaction
-let hash, itr = 0; 
 
-while (true) {
-    for (let i = 16; i < 24; i++) rand[i] = parseInt(Math.floor(Math.random() * 255)); // random uint8 (16-24) from 0-255
-    hash = createHash('sha256').update(rand).digest('hex');
-    if ( hash.substr(0, 4) == `0000` && hash[4] <= difficulty ) break;
-    itr++
+let hash, itr = 0; 
+const arr = new Uint8Array(24);
+for (let i = 0; i < 8; i++) arr[i] = accountBuffer[i];
+for (let i = 8; i < 16; i++) arr[i] = lastMineBuffer[i-8];
+
+const targetBuffer = Buffer.from(`0000${difficulty}fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff`, 'hex');
+
+while ((Buffer.compare(createHash('sha256').update(arr).digest(), targetBuffer) > 0)) {
+    itr++;
+
+    arr[16] = (itr >> 56) & 0xFF;
+    arr[17] = (itr >> 48) & 0xFF;
+    arr[18] = (itr >> 40) & 0xFF;
+    arr[19] = (itr >> 32) & 0xFF;
+    arr[20] = (itr >> 24) & 0xFF;
+    arr[21] = (itr >> 16) & 0xFF;
+    arr[22] = (itr >> 8) & 0xFF;
+    arr[23] = itr & 0xFF;
 }
+
+hash = createHash('sha256').update(arr).digest('hex');
+
 let end = Date.now();
-rand = arrayToHex(rand.slice(16, 24)); // nonce
+rand = arrayToHex(arr.slice(16, 24)); // nonce
 
 console.log(`${account} : Found hash in ${itr} nonce ${rand}, last = ${hash[4]}, hex_digest ${hash} taking ${(end - start) / 1000}s`);
 //2e1la.c.wam : Found hash in 210706 nonce 789540ca2cd7dda4, last = 0, hex_digest 000006c2e5322afd635267cedb6a5ba01307232ba594f0bc45392f816030f7eb taking 0.182s
